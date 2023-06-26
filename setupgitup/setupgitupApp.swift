e7712b57d8feca012e341b52ad164cb43e594614
Hiba Khalid <hibal>
2023-06-26 10:24:01 -0400

    Initial Commit

---
+++ setupgitup/setupgitupApp.swift
@@ -0,0 +1,17 @@
+//
+//  setupgitupApp.swift
+//  setupgitup
+//
+//  Created by AK on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct setupgitupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
